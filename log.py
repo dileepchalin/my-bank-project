35df9b3f0de227ea3598d196c6e15e98497d3e1e
dileepchalin <dileeprandi7291@gmail.com>
2022-06-10 10:20:13 +0530

    Create log.py
    <html>
    <head>

---
+++ log.py
@@ -0,0 +1,5 @@
+import math
+x=int(input("enter x"))
+y=int(input("enter y"))
+n=int(input("enter n"))
+print("z is:",math.fabs(((math.log10(math.pow(x,n))+(math.log10(math.pow(y,n))))/(((1-math.log10(math.pow(x*y,n))))))))
