8054196ae70bc337a40c1c7f5e6cdd3046ecf42c
dileepchalin <107167983+dileepchalin@users.noreply.github.com>
2022-06-10 10:41:51 +0530

    Update log.py
    adding additional information on this folder

--- log.py
+++ log.py
@@ -3,3 +3,6 @@ x=int(input("enter x"))
 y=int(input("enter y"))
 n=int(input("enter n"))
 print("z is:",math.fabs(((math.log10(math.pow(x,n))+(math.log10(math.pow(y,n))))/(((1-math.log10(math.pow(x*y,n))))))))
+log py 
+<head>
+<body>
